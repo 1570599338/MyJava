febf0d30aadcbba03bba8ae56bb4bb088357ab86
1570599338@qq.com <1570599338@qq.com>
2017-11-14 19:23:24 +0800

    treeMap和treeset

---
+++ src/com/lquan/collection/map/test/TreeMapDemo.java
@@ -0,0 +1,11 @@
+package com.lquan.collection.map.test;
+
+import java.util.TreeMap;
+
+public class TreeMapDemo {
+
+	public static void main(String[] args) {
+		TreeMap<String, String> treeMap = new TreeMap<String, String>();
+	}
+
+}
---
+++ src/com/lquan/collection/set/test/TreeSetDemo.java
@@ -0,0 +1,19 @@
+package com.lquan.collection.set.test;
+
+import java.util.TreeSet;
+
+/**
+ * Title   TreeSetDemo.java
+ * Descr   TreeSet是实现SortedSet
+ * Email   lquan.liu@outlook.com 
+ * @author lquan
+ * @dates  2017年11月13日 下午10:10:05
+ */
+public class TreeSetDemo {
+
+	public static void main(String[] args) {
+//		TreeSet<String>
+
+	}
+
+}
