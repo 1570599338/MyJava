7993eb6d41e915e295842eeb77c0c44b9844b64e
1570599338@qq.com <1570599338@qq.com>
2017-11-16 01:39:55 +0800

    Arraylist的底层代码

---
+++ src/com/lquan/collection/list/test/TestArrayListDemo.java
@@ -0,0 +1,22 @@
+package com.lquan.collection.list.test;
+
+import java.util.ArrayList;
+
+/**
+ * Title   TestArrayListDemo.java
+ * Descr   针对list的ArrayList的底层的实现
+ * Email   lquan.liu@outlook.com 
+ * @author lquan
+ * @dates  2017年11月15日 下午1:21:28
+ */
+public class TestArrayListDemo {
+	//容锐峰 15011467422    // 陈世阳 18518470030
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> list = new ArrayList<String>();
+		
+		
+
+	}
+
+}
