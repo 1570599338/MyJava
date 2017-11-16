7860537cf74af386dbc9739499beb3c8c35d2ad9
1570599338@qq.com <1570599338@qq.com>
2017-11-17 02:00:15 +0800

    io之File

---
+++ src/com/lquan/collection/io/test/FileDemo.java
@@ -0,0 +1,24 @@
+package com.lquan.collection.io.test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+/**
+ * Title   FileDemo.java
+ * Descr   TODO
+ * Email   lquan.liu@outlook.com 
+ * @author lquan
+ * @dates  2017年11月16日 上午10:01:43
+ */
+public class FileDemo {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		File file = new File("");
+		
+		FileInputStream fileInputStream =new FileInputStream(file);
+
+	}
+
+}
