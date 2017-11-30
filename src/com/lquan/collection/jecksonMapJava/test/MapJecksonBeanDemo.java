6ae75696d1b3363fb98556902275308c58cfde65
1570599338@qq.com <1570599338@qq.com>
2017-11-30 14:27:50 +0800

    数据转换

---
+++ src/com/lquan/collection/jecksonMapJava/test/MapJecksonBeanDemo.java
@@ -0,0 +1,12 @@
+package com.lquan.collection.jecksonMapJava.test;
+
+/**
+ * 
+ * <p>Title:       MapJecksonBeanDemo</p>
+ * <p>Description: json数据更改成map类型</p>
+ * <p>Created by  lquan on 2017年11月30日下午2:26:40 </p>
+ *<p>@email:  </p>
+ */
+public class MapJecksonBeanDemo {
+
+}
