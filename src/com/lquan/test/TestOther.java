f5902b3b0d40e81be79645a8dca91ca90e778066
1570599338@qq.com <1570599338@qq.com>
2017-12-27 16:13:19 +0800

    其他测试

---
+++ src/com/lquan/test/TestOther.java
@@ -0,0 +1,17 @@
+package com.lquan.test;
+
+import javax.print.DocFlavor.STRING;
+
+public class TestOther {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str;
+		//System.out.println(str);
+		M m = new M();
+		//Object
+		
+		//javax.servlet.include.request_uri = null;
+	}
+
+}
