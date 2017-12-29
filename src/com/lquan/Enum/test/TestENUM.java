8f7b55771ed789dba425660c7b8b358b335f4984
1570599338@qq.com <1570599338@qq.com>
2017-12-29 18:32:58 +0800

    枚举

---
+++ src/com/lquan/Enum/test/TestENUM.java
@@ -0,0 +1,11 @@
+package com.lquan.Enum.test;
+
+/**
+ * <p>Title:       TestENUM</p>
+ * <p>Description: 测试的第一个枚举方法</p>
+ * <p>Created by  lquan on Dec 29, 20176:30:11 PM </p>
+ *<p>@email:  lquan.liu@outlook.com</p>
+ */
+public enum TestENUM {
+
+}
